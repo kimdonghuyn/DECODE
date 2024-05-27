dd157b96ec9c3ef39aaaea4029b58175597ac70b
kimdonghuyn <say3356@naver.com>
2024-05-27 14:48:40 +0900

    [Init] 프로젝트 세팅

---
+++ src/test/java/com/example/mailauth_practice/MailAuthPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mailauth_practice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailAuthPracticeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
