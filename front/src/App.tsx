bcce049c6e6eb1545e4eb6b1e3456d28be342473
kimdonghuyn <say3356@naver.com>
2024-05-28 12:57:41 +0900

    [Feature] InputBox 컴포넌트 속성 및 공통 스타일 작성

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -1,13 +1,28 @@
-import React from 'react';
+import React, {ChangeEvent, useState} from 'react';
 import './App.css';
 import InputBox from "./components/InputBox";
 
 function App() {
-  return (
-      <>
-        <InputBox />
-      </>
-  );
+    const [id, setId] = useState<string>('');
+    const onIdChangeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+        const {value} = event.target;
+        setId(value);
+    }
+
+    const onIdButtonClickHandler = () => {
+
+    }
+
+    return (
+        <>
+            {/*<InputBox title='아이디' placeholder='아이디를 입력해주세요.' type='text' value={id} onChange={onIdChangeHandler}
+                      buttonTitle='중복 확인' onButtonClick={onIdButtonClickHandler} message='사용 가능한 아이디 입니다.'
+                      isErrorMessage={false}/>*/}
+            <div className='text-link-lg full-width'>회원가입</div>
+            <div className='kakao-sign-in-button'></div>
+            <div className='naver-sign-in-button'></div>
+        </>
+    );
 }
 
 export default App;
--- front/src/components/InputBox/index.tsx
+++ front/src/components/InputBox/index.tsx
@@ -1,20 +1,34 @@
-import {forwardRef} from "react";
+import {ChangeEvent, forwardRef, KeyboardEvent} from "react";
 import './style.css';
 
 interface Props {
-
+    title: string;
+    placeholder: string;
+    type: 'text' | 'password';
+    value: string;
+    message?: string;
+    isErrorMessage?: boolean;
+    buttonTitle?: string;
+    onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+    onKeyDown?: (event: KeyboardEvent<HTMLInputElement>) => void;
+    onButtonClick?: () => void;
 }
 
 const InputBox = forwardRef<HTMLInputElement, Props>((props: Props, ref) => {
+    const {title, placeholder, type, value,message, isErrorMessage, buttonTitle, onChange, onKeyDown, onButtonClick} = props;
+
+    const buttonClass = value === '' ? 'input-box-button-disable' : 'input-box-button';
+    const messageClass = isErrorMessage ? 'input-box-message-error' : 'input-box-message';
+
     return (
         <div className='input-box'>
-            <div className='input-box-title'>{'아이디'}</div>
+            <div className='input-box-title'>{title}</div>
             <div className='input-box-content'>
                 <div className='input-box-body'>
-                    <input className='input-box-input' placeholder='아이디를 입력해주세요'/>
-                    <div className='input-box-button'>{'중복 확인'}</div>
+                    <input ref={ref} className='input-box-input' placeholder={placeholder} type={type} value={value} onChange={onChange} onKeyDown={onKeyDown} />
+                    {buttonTitle !== undefined && onButtonClick !== undefined && <div className={buttonClass} onClick={onButtonClick}>{buttonTitle}</div>}
                 </div>
-                    <div className='input-box-message'>{'사용 가능한 아이디 입니다.'}</div>
+                {message !== undefined && <div className={messageClass}>{message}</div>}
             </div>
         </div>
     );
