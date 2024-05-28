16bb013d2e7ff7561f71b10b3e8ced5b9c2a17ac
kimdonghuyn <say3356@naver.com>
2024-05-28 15:41:11 +0900

    [Feature] ValidationExceptionHandler / JwtProvider 작성

---
+++ src/main/java/com/example/mailauth_practice/handler/ValidationExceptionHandler.java
@@ -0,0 +1,16 @@
+package com.example.mailauth_practice.handler;
+
+import com.example.mailauth_practice.dto.response.ResponseDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class ValidationExceptionHandler {
+    @ExceptionHandler({MethodArgumentNotValidException.class, HttpMessageNotReadableException.class})
+    public ResponseEntity<ResponseDto> validationExceptionHandler(Exception exception) {
+        return ResponseDto.validationFail();
+    }
+}
